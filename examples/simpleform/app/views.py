6ddbd28fc455694ad7c5129f5e4a9da4a1a15bf4
Sophia Deng <hustdengbaoqin@gmail.com>
2024-08-06 21:24:49 -0400

    Add api results

--- examples/simpleform/app/views.py
+++ examples/simpleform/app/views.py
@@ -4,6 +4,7 @@ from flask_babel import lazy_gettext as _
 
 from . import appbuilder, db
 from .forms import MyForm
+import requests
 
 
 class MyFormView(SimpleFormView):
@@ -13,7 +14,15 @@ class MyFormView(SimpleFormView):
 
     def form_get(self, form):
         form.field1.data = "This was prefilled"
-
+        api_url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json'
+        payload = {}
+        response = requests.request("GET", api_url, data=payload, verify=False)
+        if response.status_code == 200:
+            data = str(response.json()['Results'][0]["Make_ID"])
+            # Pre-fill the form with data from the API
+            form.field1.data = data
+        else:
+            flash('Failed to retrieve data from the API', 'danger')
     def form_post(self, form):
         # post process form
         flash(self.message, "info")
